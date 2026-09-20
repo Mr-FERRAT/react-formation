a24ec4931e77d7b2689f99438b528150ca4d7ca9
FERRAT Mawloud <mawloud.ferrat@gmail.com>
2026-09-20 21:24:15 +0100

    Le destructuring

--- 02 - Rappel JavaScript moderne/1.Rappel-JavaScript-moderne/5.Destructuring/script.js
+++ 02 - Rappel JavaScript moderne/1.Rappel-JavaScript-moderne/5.Destructuring/script.js
@@ -8,29 +8,42 @@
 /* A. Affecter les propriétés d'un objet. */
 
 const userObj = {
-  userName: "Karl",
+  // userName: "Karl",
   age: 44,
-  country: "Germany"
+  country: "Germany",
+  sport: "Basketball"
 }
 
-
+// const {age, country, userName} = userObj
+// console.log(age,country,userName)
 
 
 // Donner un nom différent aux constantes
-
+// const {userNmae: mainName, age:exactAge, country: origin} = userObj
+// console.log(mainName)
 
 
 // Créer des constantes par défaut, au cas où les props n'existent pas.
-
+// const {country, sport = "Football", beverage = "Bear"} = userObj
+// console.log(country, sport, beverage)
 
 
 // Combiner un nom différent et des valeurs par défaut.
-
-
+// const {userName: mainName = "Hanz"} = userObj
+// console.log(mainName)
 
 
 // Affecter les props d'un objet attendu en paramètre
+const numbers = {
+  num1: 50,
+  num2: 100
+}
 
+function foo({num1, num2}){
+  console.log(numbers)
+  return num1 + num2
+}
+// console.log(foo(numbers))
 
 
 
@@ -38,14 +51,28 @@ const userObj = {
 
 const animals = ["cat","dog","mouse","lion"];
 
+const [cat, dog] = animals
+// console.log(cat, dog)
+
+let animal1, animal2
+[animal1,animal2] = animals
+// console.log(animal1,animal2)
 
 
 
 // valeur par défaut et ignorer des valeurs
+const fruits = ["streawberry","grapes","banana","apple"]
 
+// const [streawberry,grapes,,apple] = fruits
+// console.log(streawberry,grapes,apple)
 
-
+// const [streawberry="red fruit"] = fruits
+// console.log(streawberry)
 // affecter le reste d'un tableau
-
+const [student, ...marks]=["Adrien",12,12,9,11,12,15]
+// console.log(student, marks)
 
 // Fonctionne aussi avec les strings
+const str = "ABC"
+const [a,b,c] = str
+console.log(a,b,c)
