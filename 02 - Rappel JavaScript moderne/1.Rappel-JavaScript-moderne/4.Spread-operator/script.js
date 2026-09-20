06d00f3d3c5be2f9f7fd594aa4c0492d0f0e3ed2
FERRAT Mawloud <mawloud.ferrat@gmail.com>
2026-09-20 20:52:58 +0100

    Spread-operator

--- 02 - Rappel JavaScript moderne/1.Rappel-JavaScript-moderne/4.Spread-operator/script.js
+++ 02 - Rappel JavaScript moderne/1.Rappel-JavaScript-moderne/4.Spread-operator/script.js
@@ -5,11 +5,26 @@
     C'est pratique pour effectuer une copie superficielle ou afficher une liste.
 */
 
+const array = [1,2,3,{a:5}]
+// console.log(...array)
+
+const shallowArraycopy = [...array,4,5,6]
+array[3].a = 555
+// console.log(shallowArraycopy)
+
+const obj = {
+    name: "Laura",
+    age: 26
+}
+const shallowObjCopy = {...obj, dogNma:"Tim"}
+console.log(shallowObjCopy)
 
 // si une propriété est rajoutée alors qu'elle provient d'un objet copié, elle sera remplacée.
-
+const shallowObjCopy2 = {...obj, name:"Anna", age:99}
+console.log(shallowObjCopy2)
 
 
 // Fonctionne également avec les chaînes de caractères
 const str = "Le chat saute, le chien court."
-
+const strCoppyArray = [...str]
+console.log(strCoppyArray)
