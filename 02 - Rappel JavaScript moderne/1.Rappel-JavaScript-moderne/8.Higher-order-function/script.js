60dce21fa66d9ebe6ab497a5b9752c213c8204df
FERRAT Mawloud <mawloud.ferrat@gmail.com>
2026-09-21 21:36:25 +0100

    Les fonctions d'ordre supérieur

--- 02 - Rappel JavaScript moderne/1.Rappel-JavaScript-moderne/8.Higher-order-function/script.js
+++ 02 - Rappel JavaScript moderne/1.Rappel-JavaScript-moderne/8.Higher-order-function/script.js
@@ -3,22 +3,27 @@
 
     Les fonctions d'ordre supérieur sont des fonctions qui vont soit recevoir une fonction callback en argument soit retourner une fonction, ou les deux à la fois !
 
-    Cela ouvre la voie à de nombreuses solutions et à une plus grande fléxibilité.
+    Cela ouvre la voie à de nombreuses solutions et à une plus grande flexibilité.
 */
 
 
+
+function customFilter(arr, callback){
+    
+    const filterdArray = []
+    
+    for (let i=0;i < arr.length; i++){
+        if (callback(arr[i])){
+            filterdArray.push(arr[i])
+        }
+    }
+    return filterdArray
+}
+
 const salaries = [1200,5000,4000,2500,3450,1800]
 
-
-
-
-
-
-
-
-
-
-
+console.log(customFilter(salaries, salary => salary > 3000))
+console.log(customFilter(salaries, salary => salary > 1000))
 
 
 const countries = [
@@ -44,3 +49,4 @@ const countries = [
   },
 ]
 
+console.log(customFilter(countries, country => country.gdp >= 3000))
