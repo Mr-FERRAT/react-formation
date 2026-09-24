75d9356c4adca32243f3bc0fc90d44ec8b01c7f1
FERRAT Mawloud <mawloud.ferrat@gmail.com>
2026-09-24 21:16:28 +0100

    Utiliser les modules es6

---
+++ 02 - Rappel JavaScript moderne/1.Rappel-JavaScript-moderne/12.Modules-es6/module.js
@@ -0,0 +1,11 @@
+const age = 45
+let userName = "Giovanni"
+
+export default function foo(){
+    console.log("Hello from module")
+}
+
+export{age}
+export{userName}
+
+console.log("hello")
---
+++ 02 - Rappel JavaScript moderne/1.Rappel-JavaScript-moderne/12.Modules-es6/script.js
@@ -0,0 +1,10 @@
+// import{age, userName} from "./module.js"
+// import foo from "./module.js"
+
+// foo()
+// console.log(age, userName)
+
+// import * as Utils from "./module.js"
+// console.log(Utils)
+
+import "./module.js"
